fd61da5ae587238df647722681d83182dbbc0b70
Celine Tran <celinephoungtran@gmail.com>
2021-04-12 19:31:53 -0400

    fix testing

--- testwin.py
+++ testwin.py
@@ -21,35 +21,35 @@ def test3():
 
     # (1) diagonal right to left
     test1 = [0, 2, 1, 1, 2, 0]
-    test.testCase(test1)
+    test.testCase(test1, 3)
 
     # (2) diagonal left to right
     test2 = [0, 0, 1, 1, 2, 2]
-    test.testCase(test2)
+    test.testCase(test2, 3)
 
     #(3) left 0
     test3 = [0, 2, 0, 1, 0, 0]
-    test.testCase(test3)
+    test.testCase(test3, 3)
 
     #(4) left 1
     test4 = [1, 2, 1, 1, 1, 0]
-    test.testCase(test4)
+    test.testCase(test4, 3)
 
     #(5) left 2
     test5 = [2, 2, 2, 1, 2, 0]
-    test.testCase(test5)
+    test.testCase(test5, 3)
 
     #(6) up 0
     test6 = [0, 0, 1, 0, 2, 0]
-    test.testCase(test6)
+    test.testCase(test6, 3)
 
     #(7) up 1
     test7 = [0, 1, 1, 1, 2, 1]
-    test.testCase(test7)
+    test.testCase(test7, 3)
 
     #(8) up 2
     test8 = [0, 2, 1, 2, 2, 2]
-    test.testCase(test8)
+    test.testCase(test8, 3)
 
 def test3Rand():
     position = []
@@ -67,4 +67,6 @@ def test4Rand():
     test = TestWin(4)
     test.testCase(position, 9)
 
+test3()
 test3Rand()
+test4Rand()
